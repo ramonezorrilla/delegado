3440711f06086da252bb23fc4dbe9b43063c3e36
ramonezorrilla <ramonezorrilla@gmail.com>
2023-11-18 12:37:52 -0400

    Add project files.

---
+++ delegado/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+using System;
+using System.Reflection;
+
+public delegate void D();
+
+public class ComprobacionDelegados
+{
+    public static void Main()
+    { 
+        Type t = typeof(ComprobacionDelegados);
+        MethodInfo m = t.GetMethod("Método1");
+        D obj = (D) Delegate.CreateDelegate(typeof(D), m);
+        obj();
+    }
+
+    public static void Método1()
+    { Console.WriteLine("Ejecutado Método1");}
+
+    public static void Método2(string s)
+    { Console.WriteLine("Ejecutado Método2"); }
+
+}
